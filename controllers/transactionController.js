dc69f2697866932af44b54761490b641c36650cc
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2023-06-02 10:42:55 +0530

    formatted the code

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,65 +1,78 @@
 //controllers/transactionController.js
-const Transaction = require('../models/transaction');
+const Transaction = require("../models/transaction");
 
 // Display index page
 exports.index = (req, res) => {
-  res.render('index');
+  res.render("index");
 };
 
 // Create transaction
-exports.transaction_create = (req, res) => {
+exports.transaction_create = (req, res, next) => {
   const transaction = new Transaction({
     description: req.body.description,
     amount: req.body.amount,
     date: req.body.date,
-    type: req.body.type
+    type: req.body.type,
   });
 
-  transaction.save()
+  transaction
+    .save()
     .then(() => {
-      res.redirect('/');
+      res.redirect("/");
     })
-    .catch(err => {
+    .catch((err) => {
       next(err);
     });
 };
 
 // Display detail page for a specific transaction
-exports.transaction_detail = (req, res) => {
-  Transaction.findById(req.params.id, (err, transaction) => {
-    if (err) {
-      return next(err);
-    }
-    res.render('transaction_detail', {transaction: transaction});
-  });
+exports.transaction_detail = (req, res, next) => {
+  Transaction.findById(req.params.id)
+    .then((transaction) => {
+      if (!transaction) {
+        // Transaction not found
+        return res.status(404).send("Transaction not found");
+      }
+      res.render("transaction_detail", { transaction: transaction });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 // Display list of all transactions
-exports.transaction_list = (req, res) => {
-  Transaction.find({}, (err, transactions) => {
-    if (err) {
-      return next(err);
-    }
-    res.render('transaction_list', {transactions: transactions});
-  });
+exports.transaction_list = (req, res, next) => {
+  Transaction.find({})
+    .then((transactions) => {
+      res.render("transaction_list", { transactions: transactions });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 // Update
-exports.transaction_update = (req, res) => {
-    Transaction.findByIdAndUpdate(req.params.id, {$set: req.body}, (err, transaction) => {
-      if (err) {
-        return next(err);
+exports.transaction_update = (req, res, next) => {
+  Transaction.findByIdAndUpdate(req.params.id, { $set: req.body })
+    .then((transaction) => {
+      if (!transaction) {
+        // Transaction not found
+        return res.status(404).send("Transaction not found");
       }
       res.redirect(transaction.url);
+    })
+    .catch((err) => {
+      next(err);
     });
-  };
-  
-  // Delete
-  exports.transaction_delete = (req, res) => {
-    Transaction.findByIdAndRemove(req.params.id, (err) => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect('/');
+};
+
+// Delete
+exports.transaction_delete = (req, res, next) => {
+  Transaction.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      next(err);
     });
-  };
+};
--- index.js
+++ index.js
@@ -1,45 +1,43 @@
-//Create a web-based personal finance tracker using JavaScript, HTML, and CSS. The application should allow users to add, edit, and delete income and expense transactions and display the current balance. 
+//Create a web-based personal finance tracker using JavaScript, HTML, and CSS. The application should allow users to add, edit, and delete income and expense transactions and display the current balance.
 //give me the complete code for the index.js file
 //index.js
 // Require node modules
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
 // Set up body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set up static directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // set up mongoose connection
-const mongoose = require('mongoose');
-const mongoDBUrl = 'mongodb+srv://arnavprakash:nicedatabase123@cluster1.bbkvpc6.mongodb.net/?retryWrites=true&w=majority'; // Replace with your MongoDB connection string
+const mongoose = require("mongoose");
+const mongoDBUrl =
+  "mongodb+srv://arnavprakash:nicedatabase123@cluster1.bbkvpc6.mongodb.net/?retryWrites=true&w=majority"; // Replace with your MongoDB connection string
 
-mongoose.connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
     // Continue with defining models and performing database operations
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   });
 
 // Set up routes
-const routes = require('./routes/index');
-app.use('/', routes);
+const routes = require("./routes/index");
+app.use("/", routes);
 
 // Set up view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Listen on port 3000
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });
-
-  
-
-
